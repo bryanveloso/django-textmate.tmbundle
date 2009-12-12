bf719b5067e6e5390b5cd1b5227472606a819906
Bryan Veloso <bryan@revyver.com>
2009-12-12 10:49:11 -0800

    Initial commit.

---
+++ Templates/Django TestCase Template.tmTemplate/test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from django.test import TestCase
+
+class Test%(application)s(TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass 
